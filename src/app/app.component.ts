3f1496e105f95b4286e41a645969ff17ebcc30a1
mlo00 <mamadou.lo@lmdsio.fr>
2023-11-09 17:07:34 +0100

    V1

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import {ComposantSelecteurComponent} from "./composant-selecteur/composant-selecteur.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, ComposantSelecteurComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'monProjetAngular';
+}
